cb36268f9043b1c4d7c1deda67b0cc56b98d1631
Carlos Eduardo Witte <cewitte@icloud.com>
2025-02-28 15:42:04 -0300

    replace with Swift data intro example

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,17 +5,34 @@
 //  Created by Carlos Eduardo Witte on 26/02/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
     
     var body: some View {
         NavigationStack {
-            TextEditor(text: $notes)
-                .navigationBarTitle("Bookworm")
-                .padding()
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                Button("Add") {
+                    let firstNames = ["Luke", "Leia", "Hans", "Anakin", "Darth", "Senator"]
+                    let lastNames = ["Skywalker", "Organa", "Solo", "Wader", "Sidious", "Palpatine"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    
+                    modelContext.insert(student)
+                }
+            }
         }
+        
     }
 }
 
